5ef214ac67111bbafe40e29e58d3b0d7e43cd927
Saidakbar <saidakbar20132013@gmail.com>
2025-07-12 16:11:02 +0500

    fnvd

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import "../App.css";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
+import Saidakbar from "./Components/Saidakbar";
 
 export default function App() {
   return(
     <>
       <Header />
+     
+        <Saidakbar/> 
       <Main />
       <Footer />
     </>
--- src/Components/Saidakbar.jsx
+++ src/Components/Saidakbar.jsx
@@ -1,7 +1,25 @@
+import React from 'react';
+import ghj from '/src/let.png';
+
 const Saidakbar = () => {
   return (
-    <div>
-      
+    <div className="w-[1440px] h-[900px] bg-[#323946]">
+      <h2 className='text-[45px] text-center text-[white] pt-[102px]'>Latest <span className='text-[#00EEFF]'> Project</span></h2>
+      <div className='ml-[129px] mt-[40px]'>
+        <div className='flex gap-[25px]'>
+          <img src={ghj} alt="" />
+          <img src={ghj} alt="" />
+          <img src={ghj} alt="" />
+
+        </div>
+                <div className='flex gap-[25px] mt-[25px]'>
+          <img src={ghj} alt="" />
+          <img src={ghj} alt="" />
+          <img src={ghj} alt="" />
+
+        </div>
+
+      </div>
     </div>
   )
 }
