aa0f25226b1ce064568ed67e70020c6cbd0192c7
Abdulloh2010-start <abdullohlalala@gmail.com>
2025-07-15 18:47:21 +0500

    okkk

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "../App.css";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
-import Zarina from "./Components/Zarina";
 
 export default function App() {
   return(
